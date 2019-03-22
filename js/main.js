790e28971f4797d1ebdeeb2177aa715debd86ed1
Aleksey <a.abrosimow2012@yandex.ru>
2019-03-22 09:01:34 +0300

    Доработки страниц

--- js/main.js
+++ js/main.js
@@ -13,7 +13,20 @@ $(document).ready(function() {
       $(".nav-bar").toggleClass("open-menu");
       $(".menu-main .bar").toggleClass("animate");
       // $("body").toggleClass("open-menu");
-    });  
+    }); 
+
+    $.fn.animate_Text = function() {
+      var string = this.text();
+      return this.each(function(){
+        var $this = $(this);
+      $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+      $this.find('span.new').each(function(i, el){
+      setTimeout(function(){ $(el).addClass('div_opacity'); }, 40 * i);
+    });
+    });
+    };
+    $('#example').show();
+    $('#example').animate_Text(); 
 
     $('.spoiler__link').click(function(){ 
       $(this).parent().children('div.spoiler__content').toggle('fast');
