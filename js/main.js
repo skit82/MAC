2d1d947b20a6b8bb1c651a43319ec139ef69a4c5
Aleksey <a.abrosimow2012@yandex.ru>
2019-04-09 16:54:06 +0300

    Правки страниц

--- js/main.js
+++ js/main.js
@@ -122,8 +122,8 @@ $(document).ready(function() {
     dots: true,
     infinite: true,
     speed: 300,
-    prevArrow: '<img class="left-arrow" src="img/left-arrow.png">',
-    nextArrow: '<img class="right-arrow" src="img/right-arrow.png">',
+    prevArrow: '<img class="left-arrow" src="img/left-icon.png">',
+    nextArrow: '<img class="right-arrow" src="img/right-icon.png">',
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
